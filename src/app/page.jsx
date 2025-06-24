402aba8c10509786464f51fa66375bb7aa2fdd75
Erick <erick144m@gmail.com>
2025-06-24 16:41:30 -0300

    Carrossel-Ajustes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,18 @@
-'use client';
-
+"use client";
 
 import Carrossel from "@/components/carrossel/page";
+import Navbar from "@/components/navbar/page";
 
 export default function Home() {
   return (
     <div>
       <main>
-      <Carrossel />
+        <div>
+          <Navbar />
+        </div>
+        <div className="flex justify-center items-center h-screen">
+          <Carrossel />
+        </div>
       </main>
     </div>
   );
---
+++ src/components/navbar/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full z-50 bg-[#B8C3D7]/35 backdrop-blur-lg'>
+        <nav className=' text-white'>
+            <ul className='flex gap-10 items-center p-4 font-bold'>
+                <li className='ml-10'><a href="/">Inicio</a></li>
+                <li><a href="/">Animes</a></li>
+                <li><a href="/">Filmes</a></li>
+                <li className='ml-auto mr-10'><a href="/">Entrar</a></li>
+            </ul>
+
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar;
+
